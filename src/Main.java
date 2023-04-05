a8d604f05993fa6ddcdb99847fe06dea20333e15
Artnes <2023java@mail.ru>
2023-04-05 17:33:53 +0300

    Домашнее задание 10 - готовое

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       task1();
+       task2();
+       task3();
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника — " + fullName1);
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        String fullName1 = fullName.replace("ё", "е");
+        System.out.println("ФИО сотрудника — " + fullName1);
     }
 }
